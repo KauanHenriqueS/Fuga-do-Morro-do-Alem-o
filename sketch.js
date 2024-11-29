0ef25dc3b724d4b29a8aa6c1cb2f9c2ea1914d9f
KauanHenriqueS <00001098845821sp@al.educacao.sp.gov.br>
2024-11-29 15:58:44 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  musica.play();
+  musica.loop();
+}
+
+const avança = document.querySelectorALL(".proximo");
+avança.forEach(button => {
+  button.addEventListener('click', function(){
+    const atual = document.querySelector('.ativo');
+    const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+    atual.classList.remove('ativo');
+    
+    document.getElementById(proximoPasso).classList.add('ativo');
+  })
+})
+
